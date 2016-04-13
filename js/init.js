eac90408447adea5639c7cbe54c42eff3ab981ce
Ellen <ellenh@iastate.edu>
2016-04-12 23:03:49 -0500

    mobile update, hopefully

--- js/init.js
+++ js/init.js
@@ -11,29 +11,29 @@
 })(jQuery); // end of jQuery name space
 
 
-// parallax initialization
-$(document).ready(function(){
-    $('.parallax').parallax();
-});
-
-(function($){
-    $(function(){
-
-        $('.button-collapse').sideNav();
-        $('.parallax').parallax();
-        $('.modal-trigger').leanModal()
-
-        // Init search
-        $('#toggle-search').click(function() {
-            $('#main').hide();
-            $('#search').show();
-
-            $('#search-input').focus();
-            $('#search-input').focusout(function() {
-                $('#search').hide();
-                $('#main').show();
-            });
-        });
-
-    }); // end of document ready
-})(jQuery); // end of jQuery name space
+//// parallax initialization
+//$(document).ready(function(){
+//    $('.parallax').parallax();
+//});
+//
+//(function($){
+//    $(function(){
+//
+//        $('.button-collapse').sideNav();
+//        $('.parallax').parallax();
+//        $('.modal-trigger').leanModal()
+//
+//        // Init search
+//        $('#toggle-search').click(function() {
+//            $('#main').hide();
+//            $('#search').show();
+//
+//            $('#search-input').focus();
+//            $('#search-input').focusout(function() {
+//                $('#search').hide();
+//                $('#main').show();
+//            });
+//        });
+//
+//    }); // end of document ready
+//})(jQuery); // end of jQuery name space
